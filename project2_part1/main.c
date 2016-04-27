4f13427a6b6684a662a64238e923a4adc1a1adfd
harishk93 <harishk93@live.com>
2016-04-27 12:04:09 -0400

    Working

--- project2_part1/main.c
+++ project2_part1/main.c
@@ -3,9 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <sys/time.h>
+#include <xmmintrin.h>
+#include <emmintrin.h>
+#include <pmmintrin.h>
+#include <tmmintrin.h> 
+#include <smmintrin.h>
+#include <nmmintrin.h>
+#include <ammintrin.h> 
+#include <x86intrin.h>
 #include "p2random.h"
 #include "tree.h"
+
 
 int main(int argc, char* argv[]) {
         // parsing arguments
@@ -19,7 +28,7 @@ int main(int argc, char* argv[]) {
                 fanout[i] = strtoull(argv[i + 3], NULL, 0);
                 assert(fanout[i] >= 2 && fanout[i] <= 17);
         }
-
+			
         // building the tree index
         rand32_t* gen = rand32_init((uint32_t) time(NULL));
         assert(gen != NULL);
@@ -32,33 +41,51 @@ int main(int argc, char* argv[]) {
                 free(gen);
                 exit(EXIT_FAILURE);
         }
-		
-		for(int i =0; i < tree->node_capacity[0]*tree->node_capacity[1]; i++)
-		{
-			printf("Level 1, %d: %d \n",tree->key_array[1][i],i);
-		}
+
 
         // generate probes
         int32_t* probe = generate(num_probes, gen);
         assert(probe != NULL);
         free(gen);
         uint32_t* result = malloc(sizeof(uint32_t) * num_probes);
+		uint32_t* result1 = malloc(sizeof(uint32_t) * num_probes);
+		uint32_t* result2 = malloc(sizeof(uint32_t) * num_probes);
         assert(result != NULL);
-
-        // perform index probing (Phase 2)
-        /*for (size_t i = 0; i < num_probes; ++i) {
-                result[i] = probe_index(tree, probe[i]);
-        }*/
 		
-		probe_harcoded(tree,probe[0]);
+        // perform index probing (Phase 2)
+		struct timeval stop_1, stop_2, start_1, start_2, start_3, stop_3;
 
-        // output results
-      /*  for (size_t i = 0; i < num_probes; ++i) {
-                fprintf(stdout, "%d %u\n", probe[i], result[i]);
-        } */ 
+		gettimeofday(&start_1, NULL);
+		for (size_t i = 0; i < num_probes; ++i) {
+                result[i] = probe_index(tree, probe[i]);
+        }
+		gettimeofday(&stop_1, NULL);	
 
-        // cleanup and exit
+		gettimeofday(&start_2, NULL);
+		for (size_t i = 0; i < num_probes; ++i) {
+				result1[i] = probe_index2(tree,probe[i]);
+				result2[i]=result1[i];
+        }
+		gettimeofday(&stop_2, NULL);	
+		gettimeofday(&start_3, NULL);
+		if(tree->num_levels==3 && (tree->node_capacity[0]==8) && (tree->node_capacity[1]==4) && (tree->node_capacity[2]==8))
+		{
+			for (size_t i = 0; i < num_probes/4; ++i) {
+				__m128i k=_mm_load_si128((__m128i*)&probe[4*i]);
+				probe_hardcoded(tree,k,result2,4*i);
+			}
+		}
+		gettimeofday(&stop_3, NULL);	
+		for (size_t i = 0; i < num_probes; ++i) {
+                fprintf(stdout, "%d:%d %u %u %u\n", i,probe[i], result[i], result1[i], result2[i]);
+        } 
+		printf("Method 1 took %lu.\n", stop_1.tv_usec - start_1.tv_usec);
+		printf("Method 2 took %lu.\n", stop_2.tv_usec - start_2.tv_usec);
+		printf("Method 3 took %lu.\n", stop_3.tv_usec - start_3.tv_usec);
+		printf("By default the third column is equal to the second and is made equal to the results of the hardcoded implementation only in the case of 9-5-9\n");
         free(result);
+		free(result1);
+		free(result2);
 		free(probe);
         cleanup_index(tree);
         return EXIT_SUCCESS;
--- project2_part1/tree.c
+++ project2_part1/tree.c
@@ -128,53 +128,191 @@ uint32_t probe_index(Tree* tree, int32_t probe_key) {
         return (uint32_t) result;
 }
 
-/*uint32_t probe_index2(Tree* tree, int32_t probe_key)
+uint32_t probe_index2(Tree* tree, int32_t probe_key)
 {
     size_t n = tree->num_levels;
 	size_t i = 0; 
-	for (i=0; i<n; i++)
-	{
-		size_t capacity = tree->node_capacity[i];
-		
-	}
-	     	
-}*/
-
-
-uint32_t probe_harcoded(Tree* tree, int32_t probe_key)
-{
-	size_t result = 0; 
-	printf("probe:%d\n",probe_key);
-	
-	//Loading of Probe Key into a 128 register
+	int result=0, prev_result=0;
 	__m128i key = _mm_cvtsi32_si128(probe_key);
 	key = _mm_shuffle_epi32(key,_MM_SHUFFLE(0,0,0,0));
-	
+	for (i=0; i<n; i++)
+	{
+		prev_result=result;
+		size_t capacity = tree->node_capacity[i];
+		if(capacity==8 || capacity==9)
+		{
+			__m128i lvl_2_a = _mm_load_si128(&(tree->key_array[i][prev_result << 3])); 
+			__m128i lvl_2_b = _mm_load_si128(&(tree->key_array[i][(prev_result << 3) + 4])); 
+			
+			__m128i cmp_2_a = _mm_cmplt_epi32(lvl_2_a,key);
+			__m128i cmp_2_b = _mm_cmplt_epi32(lvl_2_b,key);
+			
+			__m128i cmp_2 = _mm_packs_epi32(cmp_2_a,cmp_2_b);
+			cmp_2 = _mm_packs_epi16(cmp_2,_mm_setzero_si128());
+			
+			result = _mm_movemask_epi8(cmp_2);
+			result=_bit_scan_forward(result^ 0x1FFFF);
+			result+=(prev_result<<3)+prev_result;
+		}
+		else if(capacity==4 || capacity==5)
+		{
+			__m128i lvl_1 = _mm_load_si128(&(tree->key_array[i][prev_result << 2])); 
+				
+			__m128i cmp_1 = _mm_cmplt_epi32(lvl_1,key);
+				
+			result = _mm_movemask_ps((__m128)cmp_1);
+			result=_bit_scan_forward(result^ 0x1FF);
+
+			result+=(prev_result<<2)+prev_result;
+		}
+		else if(capacity==17 || capacity==16)
+		{
+			__m128i lvl_2_a = _mm_load_si128(&(tree->key_array[i][prev_result << 4])); 
+			__m128i lvl_2_b = _mm_load_si128(&(tree->key_array[i][(prev_result << 4)+4])); 
+			__m128i lvl_2_c = _mm_load_si128(&(tree->key_array[i][(prev_result << 4)+8])); 
+			__m128i lvl_2_d = _mm_load_si128(&(tree->key_array[i][(prev_result << 4)+12]));
+
+			__m128i cmp_2_a = _mm_cmplt_epi32(lvl_2_a,key);
+			__m128i cmp_2_b = _mm_cmplt_epi32(lvl_2_b,key);
+			__m128i cmp_2_c = _mm_cmplt_epi32(lvl_2_c,key);
+			__m128i cmp_2_d = _mm_cmplt_epi32(lvl_2_d,key);
+
+			__m128i cmp_2_ab = _mm_packs_epi32(cmp_2_a,cmp_2_b);
+			__m128i cmp_2_cd = _mm_packs_epi32(cmp_2_c,cmp_2_d);
+			
+			__m128i cmp_2 = _mm_packs_epi16(cmp_2_ab,cmp_2_cd);
+			
+			result = _mm_movemask_epi8(cmp_2);
+				//printf("Result:%d\n",result);
+			result=_bit_scan_forward(result ^ 0x1FFFFFF);
+			result+=(prev_result<<4)+prev_result;
+				//printf("r_17:%d\n",result);
+		}
+	}  
+	return result;
+}
+void probe_hardcoded(Tree* tree, __m128i k, uint32_t* result2, uint32_t start)
+{
+	register __m128i k1=_mm_shuffle_epi32(k,_MM_SHUFFLE(0,0,0,0));
+	register __m128i k2=_mm_shuffle_epi32(k,_MM_SHUFFLE(1,1,1,1));
+	register __m128i k3=_mm_shuffle_epi32(k,_MM_SHUFFLE(2,2,2,2));
+	register __m128i k4=_mm_shuffle_epi32(k,_MM_SHUFFLE(3,3,3,3));
+
 	//Loading of Level 0 into Register - 9 Way
-	__m128i cmp_0_a = _mm_cmplt_epi32(lvl_0_a,key);
-	__m128i cmp_0_b = _mm_cmplt_epi32(lvl_0_b,key);
-	__m128i cmp_0 = _mm_packs_epi32(cmp_0_a,cmp_0_b);
-	cmp_0 = _mm_packs_epi16(cmp_0,_mm_setzero_si128());
-	int r_0 = _mm_movemask_epi8(cmp_0);
-			//print128_num(cmp_0_a);
-			//print128_num(cmp_0_b);
-			//print128_num(cmp_0);
-	r_0=_bit_scan_forward(r_0^ 0x1FFFF);
-	printf("r_0:%d\n",r_0);
+	__m128i cmp_01_a = _mm_cmplt_epi32(lvl_0_a,k1);
+	__m128i cmp_01_b = _mm_cmplt_epi32(lvl_0_b,k1);
+	
+	__m128i cmp_02_a = _mm_cmplt_epi32(lvl_0_a,k2);
+	__m128i cmp_02_b = _mm_cmplt_epi32(lvl_0_b,k2);
+	
+	__m128i cmp_03_a = _mm_cmplt_epi32(lvl_0_a,k3);
+	__m128i cmp_03_b = _mm_cmplt_epi32(lvl_0_b,k3);
+	
+	__m128i cmp_04_a = _mm_cmplt_epi32(lvl_0_a,k4);
+	__m128i cmp_04_b = _mm_cmplt_epi32(lvl_0_b,k4);
+	
+	__m128i cmp_01 = _mm_packs_epi32(cmp_01_a,cmp_01_b);
+	cmp_01 = _mm_packs_epi16(cmp_01,_mm_setzero_si128());
+	
+	__m128i cmp_02 = _mm_packs_epi32(cmp_02_a,cmp_02_b);
+	cmp_02 = _mm_packs_epi16(cmp_02,_mm_setzero_si128());
+	
+	__m128i cmp_03 = _mm_packs_epi32(cmp_03_a,cmp_03_b);
+	cmp_03 = _mm_packs_epi16(cmp_03,_mm_setzero_si128());
+	
+	__m128i cmp_04 = _mm_packs_epi32(cmp_04_a,cmp_04_b);
+	cmp_04 = _mm_packs_epi16(cmp_04,_mm_setzero_si128());
+	
+	int r_01 = _mm_movemask_epi8(cmp_01);
+	int r_02 = _mm_movemask_epi8(cmp_02);
+	int r_03 = _mm_movemask_epi8(cmp_03);
+	int r_04 = _mm_movemask_epi8(cmp_04);
+	
+	r_01=_bit_scan_forward(r_01^ 0x1FFFF);
+	r_02=_bit_scan_forward(r_02^ 0x1FFFF);
+	r_03=_bit_scan_forward(r_03^ 0x1FFFF);
+	r_04=_bit_scan_forward(r_04^ 0x1FFFF);
 	
 	//Level 0 Processing Ends here and Level 1 Begins
-	//Leveel 1 Processing - 5 way
-	__m128i lvl_1 = _mm_load_si128(&(tree->key_array[1][r_0 << 2])); 
-		print128_num(lvl_1);
-	__m128i cmp_1 = _mm_cmplt_epi32(lvl_1,key);
-		print128_num(cmp_1);
-	int r_1 = _mm_movemask_epi8(cmp_1);
-	r_1=_bit_scan_forward(r_1^ 0x1FF);
-	printf("r_1:%d\n",r_1);
-	r_1+=(r_0<<2)+r_0;
-	printf("r_1:%d\n",r_1);
+	//Level 1 Processing - 5 way
+	__m128i lvl_11 = _mm_load_si128(&(tree->key_array[1][r_01 << 2])); 
+	__m128i lvl_12 = _mm_load_si128(&(tree->key_array[1][r_02 << 2])); 
+	__m128i lvl_13 = _mm_load_si128(&(tree->key_array[1][r_03 << 2])); 
+	__m128i lvl_14 = _mm_load_si128(&(tree->key_array[1][r_04 << 2])); 
 	
+	__m128i cmp_11 = _mm_cmplt_epi32(lvl_11,k1);
+	__m128i cmp_12 = _mm_cmplt_epi32(lvl_12,k2);
+	__m128i cmp_13 = _mm_cmplt_epi32(lvl_13,k3);
+	__m128i cmp_14 = _mm_cmplt_epi32(lvl_14,k4);
 	
+	int r_11 = _mm_movemask_ps((__m128)cmp_11);
+	int r_12 = _mm_movemask_ps((__m128)cmp_12);
+	int r_13 = _mm_movemask_ps((__m128)cmp_13);
+	int r_14 = _mm_movemask_ps((__m128)cmp_14);
+	
+	r_11=_bit_scan_forward(r_11^ 0x1FF);
+	r_12=_bit_scan_forward(r_12^ 0x1FF);
+	r_13=_bit_scan_forward(r_13^ 0x1FF);
+	r_14=_bit_scan_forward(r_14^ 0x1FF);
+
+	r_11+=(r_01<<2)+r_01;
+	r_12+=(r_02<<2)+r_02;
+	r_13+=(r_03<<2)+r_03;
+	r_14+=(r_04<<2)+r_04;
+			
+	//Level 1 Processing Ends here and Level 2 Begins
+	//Level 2 Processing - 9 way
+	__m128i lvl_21_a = _mm_load_si128(&(tree->key_array[2][r_11 << 3])); 
+	__m128i lvl_21_b = _mm_load_si128(&(tree->key_array[2][(r_11 << 3) + 4])); 
+	__m128i lvl_22_a = _mm_load_si128(&(tree->key_array[2][r_12 << 3])); 
+	__m128i lvl_22_b = _mm_load_si128(&(tree->key_array[2][(r_12 << 3) + 4])); 
+	__m128i lvl_23_a = _mm_load_si128(&(tree->key_array[2][r_13<< 3])); 
+	__m128i lvl_23_b = _mm_load_si128(&(tree->key_array[2][(r_13 << 3) + 4])); 
+	__m128i lvl_24_a = _mm_load_si128(&(tree->key_array[2][r_14<< 3])); 
+	__m128i lvl_24_b = _mm_load_si128(&(tree->key_array[2][(r_14 << 3) + 4])); 
+
+	__m128i cmp_21_a = _mm_cmplt_epi32(lvl_21_a,k1);
+	__m128i cmp_21_b = _mm_cmplt_epi32(lvl_21_b,k1);
+
+	__m128i cmp_22_a = _mm_cmplt_epi32(lvl_22_a,k2);
+	__m128i cmp_22_b = _mm_cmplt_epi32(lvl_22_b,k2);
+
+	__m128i cmp_23_a = _mm_cmplt_epi32(lvl_23_a,k3);
+	__m128i cmp_23_b = _mm_cmplt_epi32(lvl_23_b,k3);
+
+	__m128i cmp_24_a = _mm_cmplt_epi32(lvl_24_a,k4);
+	__m128i cmp_24_b = _mm_cmplt_epi32(lvl_24_b,k4);
+
+	__m128i cmp_21 = _mm_packs_epi32(cmp_21_a,cmp_21_b);
+	__m128i cmp_22 = _mm_packs_epi32(cmp_22_a,cmp_22_b);
+	__m128i cmp_23 = _mm_packs_epi32(cmp_23_a,cmp_23_b);
+	__m128i cmp_24 = _mm_packs_epi32(cmp_24_a,cmp_24_b);
+
+	cmp_21 = _mm_packs_epi16(cmp_21,_mm_setzero_si128());
+	cmp_22 = _mm_packs_epi16(cmp_22,_mm_setzero_si128());
+	cmp_23 = _mm_packs_epi16(cmp_23,_mm_setzero_si128());
+	cmp_24 = _mm_packs_epi16(cmp_24,_mm_setzero_si128());
+
+	int r_21 = _mm_movemask_epi8(cmp_21);
+	r_21=_bit_scan_forward(r_21^ 0x1FFFF);
+	r_21+=(r_11<<3)+r_11;
+
+	int r_22 = _mm_movemask_epi8(cmp_22);
+	r_22=_bit_scan_forward(r_22^ 0x1FFFF);
+	r_22+=(r_12<<3)+r_12;
+
+	int r_23 = _mm_movemask_epi8(cmp_23);
+	r_23=_bit_scan_forward(r_23^ 0x1FFFF);
+	r_23+=(r_13<<3)+r_13;
+
+	int r_24 = _mm_movemask_epi8(cmp_24);
+	r_24=_bit_scan_forward(r_24^ 0x1FFFF);
+	r_24+=(r_14<<3)+r_14;
+
+	*(result2+start+0)=r_21;
+	*(result2+start+1)=r_22;
+	*(result2+start+2)=r_23;
+	*(result2+start+3)=r_24;
 }
 
 void cleanup_index(Tree* tree) {
